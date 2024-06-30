dbdcb453187b87e945e2cedcb1b7d99a44c91a17
stijn.tratsaert.it <stijn.tratsaert.it@gmail.com>
2024-06-30 16:39:05 +0000

    feat:init

---
+++ .idx/dev.nix
@@ -0,0 +1,45 @@
+# To learn more about how to use Nix to configure your environment
+# see: https://developers.google.com/idx/guides/customize-idx-env
+{ pkgs, ... }: {
+  # Which nixpkgs channel to use.
+  channel = "stable-23.11"; # or "unstable"
+  # Use https://search.nixos.org/packages to find packages
+  packages = [
+    pkgs.cargo
+    pkgs.rustc
+    pkgs.rustfmt
+    pkgs.stdenv.cc
+    pkgs.nodejs_20
+  ];
+  # Sets environment variables in the workspace
+  env = {
+    RUST_SRC_PATH = "${pkgs.rustPlatform.rustLibSrc}";
+  };
+  idx = {
+    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
+    extensions = [
+      "rust-lang.rust-analyzer"
+      "tamasfe.even-better-toml"
+      "serayuzgur.crates"
+      "vadimcn.vscode-lldb"
+      "angular.ng-template"
+    ];
+    workspace = {
+      # Runs when a workspace is first created with this `dev.nix` file
+      onCreate = {
+        npm-install = "npm ci --no-audit --prefer-offline --no-progress --timing";
+      };
+      # To run something each time the workspace is (re)started, use the `onStart` hook
+    };
+    # Enable previews and customize configuration
+    previews = {
+      enable = true;
+      previews = {
+        web = {
+          command = ["npm" "run" "start" "--" "--port" "$PORT" "--host" "0.0.0.0" "--disable-host-check"];
+          manager = "web";
+        };
+      };
+    };
+  };
+}
